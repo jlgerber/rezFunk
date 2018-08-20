64edd022a1690f874407867986757057704d2e3f
jonathan gerber <jlgerber@gmail.com>
2018-08-19 20:01:06 -0700

    initial

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # rezFunk
 cmake functions for rez. The main one being a function to trigger cargo builds for rez, since cmake doesnt have native support for rust yet.
+
+To use with rez, release the package, then list as a build dependency.
+You should be able to include it via ```include(RezBuildRust)```
---
+++ package.py
@@ -0,0 +1,26 @@
+name = "rezfunk"
+
+version = "0.1.0"
+
+authors = [
+    "jgerber"
+]
+
+description = \
+    """
+    cmake functions
+    """
+
+tools = [
+]
+
+requires = [
+]
+build_requires = [
+    "cmake"
+]
+uuid = "repository.rezFunk"
+
+def commands():
+    if building:
+        env.CMAKE_MODULE_PATH.append("{root}/cmake")
